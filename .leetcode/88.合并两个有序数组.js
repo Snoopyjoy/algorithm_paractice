4a50402eaa4a289f8da33bea123febe37ae4c257
Snoopyjoy <hel13140302@126.com>
2019-12-14 23:12:32 +0800

    leetcode practice

---
+++ .leetcode/88.合并两个有序数组.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=javascript
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let i = 0;
+    let j = 0;
+    while ( i < m ) {
+        let n1 = nums[i];
+        while ( j < n) {
+
+        }
+    }
+};
+// @lc code=end
+
