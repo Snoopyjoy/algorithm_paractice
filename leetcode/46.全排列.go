7062dc59ee5c35ec0d91c7ae91ef28888dd6390c
佐罗 <hexiaoliang@innotechx.com>
2020-09-22 16:19:54 +0800

    feat: leetcode practice

--- leetcode/46.全排列.go
+++ leetcode/46.全排列.go
@@ -8,23 +8,20 @@ package leetcode.46
 
 // @lc code=start
 func permute(nums []int) [][]int {
-	ret := [][]int{}
-	if len(nums) == 0 {
-		return ret
-	}
-	if len(nums) == 1{
+	if len(nums) <= 1 {
 		return [][]int{nums}
 	}
 
-	f := nums[0]
-	children := permute(nums[1:])
-	for _, v := range children {
-		for i := 0; i < len(v) +1; i++ {
-			temp := make([]int, 0, len(v)+1)
-			temp = append(temp, v[:i]...)
-			temp = append(temp, f)
-			temp = append(temp, v[i:]...)
-			ret = append(ret, temp)
+	ret := [][]int{}
+	v := nums[0]
+	childs := permute(nums[1:])
+	for _, p := range childs {
+		for j:= 0; j < len(nums);j++{
+			a := make([]int,0,len(nums))
+			a = append(a, p[:j]...)
+			a = append(a, v)
+			a = append(a, p[j:]...)
+			ret = append(ret, a)
 		}
 	}
 	return ret
