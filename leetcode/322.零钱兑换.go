839ba35e0c7f08269f2ace87d378f991934ed835
佐罗 <hexiaoliang@innotechx.com>
2020-12-19 23:31:10 +0800

    feat: practice

--- leetcode/322.零钱兑换.go
+++ leetcode/322.零钱兑换.go
@@ -5,10 +5,24 @@
  */
 
 // @lc code=start
-import "math"
-var minAmount int = 10000
 func coinChange(coins []int, amount int) int {
-	
+	if len(coins) == 0 {
+		return -1
+	}
+	dp := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		min := -1
+		for _, c := range coins {
+			if i >= c && dp[i-c] > -1 {
+				if min == -1 || dp[i-c]+1 < min {
+					min = dp[i-c] + 1
+				}
+			}
+		}
+		dp[i] = min
+	}
+	return dp[amount]
 }
+
 // @lc code=end
 
